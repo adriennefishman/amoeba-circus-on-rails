f3cb5baa6038293dc286d81519efce815fce2065
juliataitz <juliataitz@gmail.com>
2014-07-08 19:49:05 -0400

    all but edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :amoebas
+  resources :acts
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -53,4 +56,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
